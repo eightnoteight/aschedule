3d6b17c990efaf3d59d2c1877cfb28ec1fad8ad4
Srinivas Devaki <mr.eightnoteight@gmail.com>
2016-09-03 16:07:15 +0530

    added ext module, and the tests to the same. closes #12, #5

---
+++ aschedule/ext.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta, datetime
+import asyncio
+import random
+from .api import every, once_at, JobSchedule, default_schedule_manager
+
+__all__ = ['every_day', 'every_week', 'every_monday', 'every_tuesday', 'every_wednesday',
+           'every_thursday', 'every_friday', 'every_saturday', 'every_sunday',
+           'once_at_next_monday', 'once_at_next_tuesday', 'once_at_next_wednesday',
+           'once_at_next_thursday', 'once_at_next_friday', 'once_at_next_saturday',
+           'once_at_next_sunday', 'every_random_interval']
+
+
+def every_random_interval(job, interval: timedelta, loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
+    start = loop.time()
+
+    def wait_time_gen():
+        count = 0
+        while True:
+            rand = random.randrange(round(interval.total_seconds()))
+            tmp = round(start + interval.total_seconds() * count + rand - loop.time())
+            yield tmp
+            count += 1
+
+    schedule = JobSchedule(job, wait_time_gen(), loop=loop)
+    # add it to default_schedule_manager, so that user can aschedule.cancel it
+    default_schedule_manager.add_schedule(schedule)
+    return schedule
+
+
+def every_day(job, loop=None):
+    return every(job, timedelta=timedelta(days=1), loop=loop)
+
+
+def every_week(job, loop=None):
+    return every(job, timedelta=timedelta(days=7), loop=loop)
+
+every_monday = lambda job, loop=None: _every_weekday(job, 0, loop=loop)
+every_tuesday = lambda job, loop=None: _every_weekday(job, 1, loop=loop)
+every_wednesday = lambda job, loop=None: _every_weekday(job, 2, loop=loop)
+every_thursday = lambda job, loop=None: _every_weekday(job, 3, loop=loop)
+every_friday = lambda job, loop=None: _every_weekday(job, 4, loop=loop)
+every_saturday = lambda job, loop=None: _every_weekday(job, 5, loop=loop)
+every_sunday = lambda job, loop=None: _every_weekday(job, 6, loop=loop)
+
+once_at_next_monday = lambda job, loop=None: _once_at_weekday(job, 0, loop=loop)
+once_at_next_tuesday = lambda job, loop=None: _once_at_weekday(job, 1, loop=loop)
+once_at_next_wednesday = lambda job, loop=None: _once_at_weekday(job, 2, loop=loop)
+once_at_next_thursday = lambda job, loop=None: _once_at_weekday(job, 3, loop=loop)
+once_at_next_friday = lambda job, loop=None: _once_at_weekday(job, 4, loop=loop)
+once_at_next_saturday = lambda job, loop=None: _once_at_weekday(job, 5, loop=loop)
+once_at_next_sunday = lambda job, loop=None: _once_at_weekday(job, 6, loop=loop)
+
+
+def _nearest_weekday(weekday):
+    return datetime.now() + timedelta(days=(weekday - datetime.now().weekday()) % 7)
+
+
+def _every_weekday(job, weekday, loop=None):
+    return every(job, timedelta=timedelta(days=7), start_at=_nearest_weekday(weekday), loop=loop)
+
+
+def _once_at_weekday(job, weekday, loop=None):
+    return once_at(job, _nearest_weekday(weekday), loop=loop)
---
+++ tests/testing_ext.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from unittest.mock import patch
+from datetime import timedelta
+import unittest
+import asyncio
+
+import aschedule
+
+
+class TestingExt(unittest.TestCase):
+
+    def setUp(self):
+        self.loop = asyncio.get_event_loop()
+
+    async def get_coro(self):
+        pass
+
+    def test_every_day(self):
+        self.every_patcher = patch('aschedule.ext.every')
+        self.addCleanup(self.every_patcher.stop)
+        self.every_mock = self.every_patcher.start()
+        from aschedule.ext import every_day
+
+        schedule1 = every_day(self.get_coro)
+        self.every_mock.assert_called_with(self.get_coro, timedelta=timedelta(days=1), loop=None)
+        schedule2 = every_day(self.get_coro, loop=self.loop)
+        self.every_mock.assert_called_with(self.get_coro, timedelta=timedelta(days=1), loop=self.loop)
+        self.loop.run_until_complete(asyncio.sleep(1))
+        schedule1.cancel(running_jobs=True)
+        schedule2.cancel(running_jobs=True)
+        self.loop.run_until_complete(asyncio.sleep(1))
+        self.assertEqual(2, self.every_mock.call_count)
+
+    def test_every_week(self):
+        self.every_patcher = patch('aschedule.ext.every')
+        self.addCleanup(self.every_patcher.stop)
+        self.every_mock = self.every_patcher.start()
+        from aschedule.ext import every_week
+
+        schedule1 = every_week(self.get_coro)
+        self.every_mock.assert_called_with(self.get_coro, timedelta=timedelta(days=7), loop=None)
+        schedule2 = every_week(self.get_coro, loop=self.loop)
+        self.every_mock.assert_called_with(self.get_coro, timedelta=timedelta(days=7), loop=self.loop)
+        self.loop.run_until_complete(asyncio.sleep(1))
+        schedule1.cancel(running_jobs=True)
+        schedule2.cancel(running_jobs=True)
+        self.loop.run_until_complete(asyncio.sleep(1))
+        self.assertEqual(2, self.every_mock.call_count)
+
+    def test_every_random_interval(self):
+        from aschedule.ext import every_random_interval
+
+        # scheduled executions: randrange(0, 5), randrange(5, 10), randrange(10, 15)
+        times = []
+        start = self.loop.time()
+        cancel_in_seconds = 16
+        # set the seed to avoid a scheduled execution on 16th second.
+        __import__('random').seed(38042)
+        async def record_times():
+            times.append(round(self.loop.time() - start))
+        schedule = every_random_interval(record_times, timedelta(seconds=5))
+
+        async def schedule_canceller():
+            await asyncio.sleep(cancel_in_seconds)
+            aschedule.cancel(schedule)
+
+        try:
+            self.loop.run_until_complete(
+                asyncio.gather(schedule_canceller(), schedule.future))
+        except asyncio.CancelledError:
+            pass
+
+        self.assertEqual(3, len(times))
+        for i, x in enumerate(times):
+            self.assertTrue(i * 5 <= x < (i + 1) * 5)
+
+    def tearDown(self):
+        asyncio.set_event_loop(self.loop)
